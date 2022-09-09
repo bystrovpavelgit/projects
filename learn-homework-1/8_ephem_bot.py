6848516849a2aed517730107e424c11a23d55cb1
Pavel Bystrov <pavelb@tmp.com>
2022-09-09 11:29:21 +0300

    solved ephem task 8

--- learn-homework-1/8_ephem_bot.py
+++ learn-homework-1/8_ephem_bot.py
@@ -8,13 +8,13 @@
   название планеты на английском, например /planet Mars
 * В функции-обработчике команды из update.message.text получите
   название планеты (подсказка: используйте .split())
-* При помощи условного оператора if и ephem.constellation научите
+* При помощи условного оператора if и  научите
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from from ephem import constellation
+import ephem
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -29,6 +29,29 @@ PROXY = {
     }
 }
 
+def get_planet(name):
+    """ function get_planet """
+    if name.lower() = "moon":
+        return ephem.Moon()
+    elif name.lower() = "mercury":
+        return ephem.Mercury()
+    elif name.lower() = "venus":
+        return ephem.Venus()
+    elif name.lower() = "earth":
+        return ephem.Earth()
+    elif name.lower() = "mars":
+        return ephem.Mars()
+    elif name.lower() = "jupiter":
+        return ephem.Jupiter()
+    elif name.lower() = "saturn":
+        return ephem.Saturn()
+    elif name.lower() = "uranus":
+        return ephem.Uranus()
+    elif name.lower() = "neptune":
+        return ephem.Neptune()
+    elif name.lower() = "pluto":
+        return ephem.Pluto()
+    return
 
 def greet_user(update, context):
     text = 'Вызван /start'
@@ -41,15 +64,20 @@ def talk_to_me(update, context):
     print(user_text)
     update.message.reply_text(text)
 
+
 def check_planet(update, context):
-    user_text = update.message.text.split())
-    print(user_text[-1])
-    const = constellation(user_text[-1])
-    update.message.reply_text(const)
+    user_text = update.message.text.split()
+    name = user_text[-1]
+    print(name)
+    planet = get_planet(name)
+    if planet is None:
+        return
+    result = list(ephem.constellation(planet))[1]
+    update.message.reply_text(f"{result}")
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater("КЛЮЧ", request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
